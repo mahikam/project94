9b98f3dd2da88a5a8d02b61d8d8c39e687177ef7
mahikam <76594479+mahikam@users.noreply.github.com>
2021-05-15 12:13:02 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCQPeuWL4fdzdg9QrDNvjhvX9oz7MFlWFY",
+    authDomain: "project-94-e03aa.firebaseapp.com",
+    databaseURL: "https://project-94-e03aa-default-rtdb.firebaseio.com",
+    projectId: "project-94-e03aa",
+    storageBucket: "project-94-e03aa.appspot.com",
+    messagingSenderId: "76235472650",
+    appId: "1:76235472650:web:ea1341d52b3ae8b1c9c4f3",
+    measurementId: "G-QL13CML75L"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
